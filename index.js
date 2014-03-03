97f71f009e62e7147dec3fb3e96050b8fd662cb9
David Wippel <david@trigo.at>
2014-03-03 13:58:12 +0100

    added next statements

--- index.js
+++ index.js
@@ -12,12 +12,14 @@ exports.contentpool = function(getUserName) {
 					apiKey: apiKey
 				};
 				var encryptedUserToken = cryptor.encrypt(JSON.stringify(userToken), userToken.apiKey);
-				res.cookie('contentPoolAuth', encryptedUserToken);
+				res.cookie('contentPoolAuth', encryptedUserToken);				
 			}
+			return next();
 		},
 
 		logout: function(req, res, next) {
 			res.clearCookie('contentPoolAuth');
+			return next();
 		};
 	};
 };
