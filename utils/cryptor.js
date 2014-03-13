73a08189bc5336156d39f4c0e6cb64be4817b4d3
Christian Hubinger <christian@trigo.at>
2014-03-13 14:50:40 +0100

    Generate IV on demand and prepend to cookie

--- utils/cryptor.js
+++ utils/cryptor.js
@@ -1,21 +1,23 @@
 var crypto = require('crypto'),
-	algorithm = 'aes-128-cbc',
-	initVector = new Buffer('triGoContentPool');
-
-exports.encrypt = function(string, apiKey) {
-	var key = new Buffer(apiKey);	
-	var decryptedBuffer = new Buffer(string, 'ascii');
-	var cipher = crypto.createCipheriv(algorithm, key, initVector);
-	var encoded = cipher.update(decryptedBuffer, 'ascii', 'base64');
-	encoded += cipher.final('base64');
-	return encoded;
-}
+	algorithm = 'aes-128-cbc';
 
 exports.decrypt = function(encryptedString, apiKey) {
-	var key = new Buffer(apiKey);
+	var initVector = encryptedString.substr(0,16);
+	encryptedString = encryptedString.substr(16, encryptedString.length -16);
+	var key = new Buffer(apiKey);	
 	var encryptedBase64 = new Buffer(encryptedString, 'base64');
-	var decipher = crypto.createDecipheriv(algorithm, key, initVector);
+	var decipher = crypto.createDecipheriv(algorithm, key, new Buffer( initVector ) );
 	var decoded = decipher.update(encryptedBase64, 'binary', 'ascii');
 	decoded += decipher.final('ascii');
 	return decoded;
-}
+};
+
+exports.encrypt = function(string, apiKey) {
+	var initVector = require('node-uuid')().replace(/-/g, '').substr(0,16);
+	var key = new Buffer(apiKey);	
+	var decryptedBuffer = new Buffer(string, 'ascii');
+	var cipher = crypto.createCipheriv(algorithm, key, new Buffer( initVector ));
+	var encoded = cipher.update(decryptedBuffer, 'ascii', 'base64');
+	encoded += cipher.final('base64');
+	return initVector + encoded;
+};
